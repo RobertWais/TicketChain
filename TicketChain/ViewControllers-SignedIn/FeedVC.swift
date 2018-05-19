b9c84f64bf445883d0cb1111fd4d87b9a34862a5
RobertWais <wais.robert@uwlax.edu>
2018-05-19 11:38:32 -0500

    Adding more view controllers for base of project

---
+++ TicketChain/ViewControllers-SignedIn/FeedVC.swift
@@ -0,0 +1,23 @@
+//
+//  FeedVC.swift
+//  TicketChain
+//
+//  Created by Robert Wais on 5/19/18.
+//  Copyright © 2018 Robert Wais. All rights reserved.
+//
+
+import UIKit
+
+class FeedVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
